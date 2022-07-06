b29ed4c1efd8cf04c733e8c51df64c850421e40f
SkeleKing04 <jjlucas04@bigpond.com>
2022-07-06 20:07:21 +0930

    AI and More Ground Textures AI now shoots

---
+++ Assets/Scripts/AIShooting.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class AIShooting : MonoBehaviour
+{
+    [Header("Positions")]
+    private int layerMasks;
+    [Header("Tracer")]
+    //public TrailRenderer tracer;
+    public LineRenderer lineRenderer;
+    public float trailTime;
+    public float trailWidth;
+    private Color teamColor;
+
+    public enum gunState
+    {
+        ready,
+        firing,
+        reloading,
+        switchOut,
+        swtichIn
+    };
+    [Header("State Management")]
+    public gunState state;
+    [Header("Keybinds")]
+    //public KeyCode fireKey;
+    //public KeyCode secondaryActionKey;
+    //public KeyCode reloadKey;
+    //public KeyCode switchWeaponKey;
+    private List<KeyCode> inputBuffer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMasks = 1 << 8;
+        layerMasks = ~layerMasks;
+        try
+        {
+            teamColor = GetComponent<TeamManager>().teamColor;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to find team color in team manager! Did you forget to attach the component to this object? (" + gameObject.name + ")");
+        }
+    }
+    public void readyWeapon()
+    {
+        state = gunState.ready;
+    }
+    public void Shoot(Transform fireTransform, Transform trailStartTransform)
+    {
+        //Variables
+        RaycastHit hit;
+        Vector3 startPos = fireTransform.position;
+        Vector3 endPoint = fireTransform.forward;
+        //Creates the tracer line  
+        LineRenderer line = Instantiate(lineRenderer, trailStartTransform.position, Quaternion.identity);
+        // Raycast to see if object hit in range
+        // The "50f" needs to be changed to the weapons range
+        if(Physics.Raycast(startPos, endPoint, out hit, 50f, layerMasks))
+        {   
+            Debug.DrawRay(startPos, fireTransform.forward * hit.distance, Color.green, 0.1f);
+            Debug.Log("Hit object" + hit.collider.name);
+            //Begins to set up the tracer
+            StartCoroutine(setLine(line, trailStartTransform.position, hit.point));
+            try
+            {
+                hit.collider.gameObject.GetComponent<HealthHandler>().UpdateHealth(10, gameObject, true, false);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to Update Health of " + hit.collider.gameObject.name + ". Are you missing the component?");
+            }
+        }
+        else
+        {
+            Debug.DrawRay(startPos, fireTransform.forward * 50f, Color.red, 0.1f);
+            Debug.Log("Missed object.");
+            //Begins to set up the tracer
+            StartCoroutine(setLine(line, trailStartTransform.position, fireTransform.forward * 50f));
+        }
+        // stops the gun from firing stupidly
+        state = gunState.firing;
+        Invoke(nameof(readyWeapon), 0.1f);
+    }
+    private IEnumerator setLine(LineRenderer line, Vector3 startPos, Vector3 endPoint)
+    {
+        //Sets the start and end positions of the tracers
+        line.SetPosition(0, startPos);
+        line.SetPosition(1, endPoint);
+        // Sets how long the trail is visible for
+        //this needs to be a different variable
+        // the colour here should corespond to the colour of the player's team/paint
+        line.startColor = new Color(teamColor.r, teamColor.g, teamColor.b, 1);
+        line.endColor = new Color(teamColor.r, teamColor.g, teamColor.b, 1);
+
+        float time = trailTime;
+        while (time > 0)
+        {
+            line.startWidth = trailWidth * time;
+            line.endWidth = line.startWidth;
+            time -= Time.deltaTime;
+            yield return null;
+        }
+        Destroy(line.gameObject);
+    }
+}
--- Assets/Scripts/ShooterAI.cs
+++ Assets/Scripts/ShooterAI.cs
@@ -5,21 +5,40 @@ using UnityEngine.AI;
 
 public class ShooterAI : MonoBehaviour
 {
+    // Players and Enemeies
     public List<GameObject> enemies;
     public TeamManager teamManager;
     public float AISightRange;
     public float AIStopRange;
-    private bool inCombat;
+    public enum actionState
+    {
+        idle,
+        inChase,
+        inCombat
+    };
+    public actionState state;
     public GameObject target;
     private Vector3 lastKnownTargetPos;
+    // Navigation
     private NavMeshAgent Agent;
+    public float randomMoveFloat;
+    public float randomMoveWait;
+    private bool canMove;
+    // Shooting
+    public GameObject Head;
+    public Transform fireTransform;
+    public Transform trailTransform;
+    private AIShooting shooting;
+    // Other
     private LayerMask layerMasks;
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
         layerMasks = 1 << 8;
         layerMasks = ~layerMasks;
         Agent = GetComponent<NavMeshAgent>();
+        shooting = GetComponent<AIShooting>();
         foreach(GameObject player in GameObject.FindGameObjectsWithTag("Combatant"))
         {
             if(player.GetComponent<TeamManager>().teamColor != teamManager.teamColor)
@@ -32,11 +51,49 @@ public class ShooterAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(lastKnownTargetPos, transform.up * 10, Color.yellow, 0.01f);
-        if(target == null) trackEnemies();
-        else trackTarget();
-        
+        switch(state)
+        {
+            case actionState.idle:
+                if(canMove)
+                {
+                    Invoke(nameof(doIdle),randomMoveWait);
+                    canMove = false;
+                }
+                if(target == null) trackEnemies();
+                break;
+            case actionState.inChase:
+                break;
+            case actionState.inCombat:
+                int chooseAction = Random.Range(0, 4);
+                if(chooseAction == 0 && shooting.state == AIShooting.gunState.ready)
+                {
+                    shooting.Shoot(fireTransform, trailTransform);
+                }
+                if(chooseAction == 1)
+                {
+                    //Not used ATM
+                }
+                if(shooting.state == AIShooting.gunState.ready)
+                {
+                    //Reload();
+                }
+                if(chooseAction == 2 && shooting.state == AIShooting.gunState.ready)
+                {
+                    //Switch to next weapon
+                }
+                break;
+        }
+        if (target != null) trackTarget();        
     }
+    // IDLE
+    private void doIdle()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(transform.position.x - randomMoveFloat,transform.position.x + randomMoveFloat),Random.Range(transform.position.y - randomMoveFloat,transform.position.y + randomMoveFloat),Random.Range(transform.position.z - randomMoveFloat,transform.position.z + randomMoveFloat));
+        Debug.Log(randomPos);
+        Agent.SetDestination(randomPos);
+        canMove = true;
+    }
+    // FOLLOWING AND TRACKING
     private void trackEnemies()
     {
         foreach(GameObject enemy in enemies)
@@ -46,12 +103,16 @@ public class ShooterAI : MonoBehaviour
             {
                 Debug.Log(enemy.name + " can be seen");
                 target = enemy;
+                state = actionState.inChase;
                 //inCombat = true;
             }
         }
     }
     private void trackTarget()
     {
+        if((target.transform.position - transform.position).magnitude <= 10) state = actionState.inCombat;
+        else state = actionState.inChase;
+        Debug.DrawRay(lastKnownTargetPos, transform.up * 10, Color.yellow, 0.01f);
         float distance = (target.transform.position - transform.position).magnitude;
         if(distance < AISightRange && !targetBlocked(target, target.transform.position))
         {
@@ -59,7 +120,7 @@ public class ShooterAI : MonoBehaviour
             if(target.transform.position != lastKnownTargetPos)
             {
                 lastKnownTargetPos = target.transform.position;     
-                //transform.LookAt(lastKnownTargetPos);
+                Head.transform.LookAt(lastKnownTargetPos);
                 Agent.SetDestination(lastKnownTargetPos);
             }     
         }
@@ -70,6 +131,7 @@ public class ShooterAI : MonoBehaviour
         else
         {
             target = null;
+            state = actionState.idle;
             //inCombat = false;
         }
     }
@@ -80,5 +142,10 @@ public class ShooterAI : MonoBehaviour
         if(hit.collider.gameObject == objectToHit) return false;
         else if(blocked && hit.collider.gameObject != objectToHit) return true;
         else return true;
+    }
+    // COMBAT AND SHOOTING
+    private void inputSim()
+    {
+
     }
 }
