f45af42534601d0b99b2690083096e5957c62ffe
SkeleKing04 <jjlucas04@bigpond.com>
2022-09-08 01:42:27 +0930

    Updated new camera script Script needs comenting

--- Assets/Scripts/General/CameraController.cs
+++ Assets/Scripts/General/CameraController.cs
@@ -45,8 +45,9 @@ public class CameraController : MonoBehaviour
     //public Transform objectToFollow;
     public float camSpeed, camRotSpeed, camDistance;
     public Vector2 camPos, sensitivity;
-    public Vector3 rotation;
-    public bool followTargetRotation, doMouseMovement, doControlObject;
+    public Vector3 originOffset;
+    private Vector3 rotation;
+    public bool doMouseMovement;
     private Vector3 velocity;
     // The first object in this class will be the object followed
     // Any other objects that need to be visible to the camera must be a child of this first object
@@ -54,21 +55,38 @@ public class CameraController : MonoBehaviour
     public class objectsToControl
     {
         public Transform objectTransform;
-        public bool followCameraRotation, controlXRot, controlYRot, controlZRot;
+        //Set only one object to be head, first object will be selected
+        public bool followCameraRotation, controlXRot, controlYRot, controlZRot, headObject;
     }
     public objectsToControl[] objectToFollow;
+    private int mainIndex;
     // Start is called before the first frame update
-
+    void Start()
+    {
+        updateMainIndex();
+    }
+    public void updateMainIndex()
+    {
+        for(int i = 0; i < objectToFollow.Length; i++)
+        {
+            if(objectToFollow[i].headObject)
+            {
+                mainIndex = i;
+                Debug.Log("Main index set at " + mainIndex.ToString());
+                break;
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(objectToFollow[0].objectTransform.position, objectToFollow[0].objectTransform.forward * camPos.x +  objectToFollow[0].objectTransform.up * camPos.y);
-        Debug.DrawRay(objectToFollow[0].objectTransform.position, ray.GetPoint(camDistance), Color.green, 0.1f);
+        Transform mainTransform = objectToFollow[mainIndex].objectTransform;
+        Ray ray = new Ray(mainTransform.position + originOffset.x * mainTransform.right + originOffset.y * mainTransform.up, mainTransform.forward * camPos.x  +  mainTransform.up * camPos.y);
+        Debug.DrawRay(objectToFollow[mainIndex].objectTransform.position + originOffset, ray.GetPoint(camDistance), Color.green, 0.1f);
         transform.position = Vector3.SmoothDamp(transform.position, ray.GetPoint(camDistance), ref velocity, camSpeed);
-        Vector3 lookPos = objectToFollow[0].objectTransform.position;
+        Vector3 lookPos = objectToFollow[mainIndex].objectTransform.position;
         if(!doMouseMovement)
         {
-            //transform.LookAt(lookPos);
             transform.rotation = Quaternion.Slerp(transform.rotation, objectToFollow[0].objectTransform.rotation , Time.deltaTime * camRotSpeed);
         }
         else if (doMouseMovement)
@@ -85,24 +103,25 @@ public class CameraController : MonoBehaviour
         {
             if(objectToFollow[i].followCameraRotation)
             {
-                Quaternion tempRotation = Quaternion.Euler(0,0,0);
+                Vector3 tempRotation = new Vector3(0,0,0);
                 if(objectToFollow[i].objectTransform.parent != null)
                 {
-                    tempRotation = objectToFollow[i].objectTransform.parent.rotation;
+                    tempRotation = objectToFollow[i].objectTransform.transform.eulerAngles;
                 }
+
                 if(objectToFollow[i].controlXRot)
                 {
-                    tempRotation = Quaternion.Euler(rotation.x, tempRotation.y, tempRotation.z);
+                    tempRotation = new Vector3(rotation.x, tempRotation.y, tempRotation.z);
                 }
                 if(objectToFollow[i].controlYRot)
                 {
-                    tempRotation = Quaternion.Euler(tempRotation.y, rotation.y, tempRotation.z);
+                    tempRotation = new Vector3(tempRotation.x, rotation.y, tempRotation.z);
                 }
                 if(objectToFollow[i].controlZRot)
                 {
-                    tempRotation = Quaternion.Euler(tempRotation.x, tempRotation.y, rotation.z);
-                }
-                objectToFollow[i].objectTransform.rotation = tempRotation;
+                    tempRotation = new Vector3(tempRotation.x, tempRotation.y, rotation.z);
+                }                
+                objectToFollow[i].objectTransform.eulerAngles = tempRotation;
             }
         }
         }
